c4ae6c3dc57d9978397fad94e7c79d6ecec59cee
tenK <emiliocolindres@gmail.com>
2025-03-14 17:04:56 -0700

    Restructure as a library

--- composeApp/src/iosMain/kotlin/com/carthas/ui/base/Platform.ios.kt
+++
@@ -1,9 +0,0 @@
-package com.carthas.ui.base
-
-import platform.UIKit.UIDevice
-
-class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
-}
-
-actual fun getPlatform(): Platform = IOSPlatform()
