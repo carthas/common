c48dcf41cd7758bbf5725d089d3369e99171c244
tenK <emiliocolindres@gmail.com>
2025-05-10 04:53:06 -0700

    Bump dependencies and bump version to 0.2.1
    Signed-off-by: tenK <emiliocolindres@gmail.com>

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -85,7 +85,7 @@ tasks.withType<KotlinCompile>().all {
 
 group = "com.carthas"
 val artifactId = "common"
-version = "0.2.0"
+version = "0.2.1"
 
 mavenPublishing {
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
