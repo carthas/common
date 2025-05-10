3a2f8fdf91b13718e594a8d7593e6b448463c2c6
tenK <emiliocolindres@gmail.com>
2025-05-10 05:38:36 -0700

    Bump version to 0.2.2
    Signed-off-by: tenK <emiliocolindres@gmail.com>

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -85,7 +85,7 @@ tasks.withType<KotlinCompile>().all {
 
 group = "com.carthas"
 val artifactId = "common"
-version = "0.2.1"
+version = "0.2.2"
 
 mavenPublishing {
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
