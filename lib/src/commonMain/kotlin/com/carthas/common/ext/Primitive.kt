6a2440ccdfbfbe2abc497521b7277100b9f78368
tenK <emiliocolindres@gmail.com>
2025-05-30 21:29:09 -0700

    Add contracts to isNull and isNotNull
    Signed-off-by: tenK <emiliocolindres@gmail.com>

--- lib/src/commonMain/kotlin/com/carthas/common/ext/Primitive.kt
+++ lib/src/commonMain/kotlin/com/carthas/common/ext/Primitive.kt
@@ -1,8 +1,24 @@
 package com.carthas.common.ext
 
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
 
-fun Any?.isNull() = this == null
-fun Any?.isNotNull() = this != null
+
+@OptIn(ExperimentalContracts::class)
+fun Any?.isNull(): Boolean {
+    contract {
+        returns(false) implies (this@isNull != null)
+    }
+    return this == null
+}
+
+@OptIn(ExperimentalContracts::class)
+fun Any?.isNotNull(): Boolean {
+    contract {
+        returns(true) implies (this@isNotNull != null)
+    }
+    return this != null
+}
 
 infix fun <T> T?.orElse(that: T): T = this ?: that
 inline infix fun <T> T?.orElse(that: () -> T) = this ?: that()
