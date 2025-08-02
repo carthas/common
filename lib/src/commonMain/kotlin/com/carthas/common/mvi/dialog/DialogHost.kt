a9cc2950562885af1063dde87a6cefeafdd58cad
tenK <emiliocolindres@gmail.com>
2025-08-02 11:36:33 -0700

    Add custom composable dialog management
    Signed-off-by: tenK <emiliocolindres@gmail.com>

---
+++ lib/src/commonMain/kotlin/com/carthas/common/mvi/dialog/DialogHost.kt
@@ -0,0 +1,92 @@
+package com.carthas.common.mvi.dialog
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.zIndex
+import com.carthas.common.ext.runIf
+
+
+/**
+ * Provides a host for managing and displaying dialogs using a [DialogManager].
+ *
+ * The [DialogHost] is responsible for rendering dialogs and managing their lifecycle in accordance with
+ * the state and behavior defined in [DialogManager]. It applies animations, dismissal behavior, and overlays
+ * as specified by individual [Dialog] properties.
+ *
+ * The composable [mainContent] is rendered first, followed by any active dialogs. It is wrapped in
+ * a [CompositionLocalProvider] to make the [LocalDialogManager] accessible to the composable hierarchy.
+ *
+ * @param mainContent The primary composable content to be displayed underneath the dialog overlay. Dialogs managed
+ * by [DialogManager] are rendered above this content.
+ */
+@Composable
+fun DialogHost(
+    mainContent: @Composable () -> Unit,
+) {
+    val dialogManager = remember { DialogManager() }
+
+    CompositionLocalProvider(LocalDialogManager provides dialogManager) {
+
+        // display the main content first
+        mainContent()
+
+        // display any active dialogs
+        dialogManager.dialogStack.forEachIndexed { index, dialog ->
+            val isTop = index == dialogManager.dialogStack.lastIndex
+            var visible by remember { mutableStateOf(false) }
+
+            LaunchedEffect(Unit) {
+                visible = true  // set visibility to true after the first composition to activate enter transition
+
+                dialogManager.dismissEventFlow.collect {
+                    // if this dialog has been dismissed, set visibility to false
+                    if (it.dialogKey == dialog.key) visible = false
+                }
+            }
+
+            AnimatedVisibility(
+                visible = visible,
+                enter = dialog.enterTransition,
+                exit = dialog.exitTransition,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .zIndex(index.toFloat()),
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Center,
+                ) {
+                    // dims the background, receives taps and dismisses the dialog
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color.Black.copy(alpha = 0.3f))
+                            .runIf(isTop && dialog.dismissOnClickOutside) {
+                                pointerInput(Unit) { detectTapGestures { dialogManager.dismiss() } }
+                            },
+                    )
+                    // displays the dialog
+                    Box(
+                        modifier = Modifier.pointerInput(Unit) {},
+                        contentAlignment = Center,
+                        content = { dialog.content() },
+                    )
+                }
+            }
+        }
+    }
+}
---
+++ lib/src/commonMain/kotlin/com/carthas/common/mvi/dialog/DialogManager.kt
@@ -0,0 +1,81 @@
+package com.carthas.common.mvi.dialog
+
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.staticCompositionLocalOf
+import com.carthas.common.mvi.ioBoundDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+
+
+/**
+ * A [CompositionLocal] used to provide and retrieve a [DialogManager] instance within the Compose hierarchy.
+ *
+ * [DialogManager] manages the display, dismissal, and internal state of dialogs.
+ * By default, the composition local throws an exception if accessed without a provided value.
+ * It is typically set at a higher level in the compose tree using [CompositionLocalProvider].
+ *
+ * This composition local enables features like showing or dismissing dialogs through [DialogManager],
+ * typically used in conjunction with a dialog host implementation.
+ */
+val LocalDialogManager = staticCompositionLocalOf<DialogManager> { error("No DialogManager provided") }
+
+/**
+ * Manages a stack of [Dialog]s to display in a UI component.
+ * Provides functions for adding, dismissing, and clearing dialogs.
+ */
+class DialogManager internal constructor() : CoroutineScope {
+
+    /**
+     * Represents an event triggered when a dialog is dismissed.
+     */
+    internal data class DismissEvent(val dialogKey: String)
+
+    /**
+     * The [CoroutineContext] used to delay when dismissing animated dialogs.
+     */
+    override val coroutineContext: CoroutineContext = ioBoundDispatcher
+
+    /**
+     * A private stack as a [androidx.compose.runtime.snapshots.SnapshotStateList] that holds the active [Dialog]
+     * instances managed by [DialogManager].
+     */
+    private val _dialogStack = mutableStateListOf<Dialog>()
+
+    internal val dialogStack: List<Dialog>
+        get() = _dialogStack
+
+    /**
+     * A private [MutableSharedFlow] that emits [DismissEvent]s when a dialog is dismissed.
+     */
+    private val _dismissEventFlow = MutableSharedFlow<DismissEvent>()
+
+    internal val dismissEventFlow: SharedFlow<DismissEvent>
+        get() = _dismissEventFlow
+
+    /**
+     * Adds a new [Dialog] to the list of managed dialogs.
+     */
+    fun show(dialog: Dialog) {
+        _dialogStack.add(dialog)
+    }
+
+    /**
+     * Dismisses the topmost dialog in the dialog stack.
+     */
+    fun dismiss() {
+        if (_dialogStack.isNotEmpty()) launch {
+            _dismissEventFlow.emit(DismissEvent(_dialogStack.last().key))
+            delay(_dialogStack.last().dismissDelay)
+            _dialogStack.removeLast()
+        }
+    }
+
+    /**
+     * Dismisses all dialogs managed by the [DialogManager].
+     */
+    fun dismissAll() = repeat(_dialogStack.size) { dismiss() }
+}
